6f47f6ae0582782dcba1952fa21ceeedb03e3237
Akila Mendis <godomainz@gmail.com>
2024-07-08 15:49:53 +0530

    02 - IntelliJIDEA plugin, Constructor annotations & AccessLevel | PROJECT LOMBOK | Tutorial | Java

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,18 +1,28 @@
 package org.example;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.net.URL;
 
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private Long id;
     private String title;
     private String description;
     private String country;
     private int rating;
+
+    @Getter(AccessLevel.PROTECTED)
     private URL officialSite;
     private String language;
+
+    @Getter(AccessLevel.PRIVATE)
+    @Setter(AccessLevel.PRIVATE)
+    private final int minutes = 120;
 
 }
